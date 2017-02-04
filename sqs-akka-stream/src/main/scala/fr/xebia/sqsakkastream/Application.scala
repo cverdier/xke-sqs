296bfc7a5e53fce12d7b00b2d0d1957267a6e83d
cverdier <cverdier@xebia.fr>
2017-02-04 15:47:14 +0100

    Configure queue, use default credentials chain, more logs

--- sqs-akka-stream/src/main/scala/fr/xebia/sqsakkastream/Application.scala
+++ sqs-akka-stream/src/main/scala/fr/xebia/sqsakkastream/Application.scala
@@ -3,32 +3,32 @@ package fr.xebia.sqsakkastream
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.regions.Regions.EU_WEST_2
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
 import com.amazonaws.services.sqs.model.{DeleteMessageBatchRequestEntry, Message}
 import com.typesafe.config.ConfigFactory
 
 import scala.collection.JavaConversions._
-import scala.concurrent.{ExecutionContext, ExecutionException, Future, Promise}
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.language.postfixOps
-import scala.util.Success
+import scala.util.{Success, Try}
 
 
 object Application extends App {
 
   val conf = ConfigFactory.load()
-  implicit val system = ActorSystem("cleaner", conf)
+  implicit val system = ActorSystem("xke-sqs", conf)
   implicit val materializer = ActorMaterializer()
   implicit val ec: ExecutionContext = system.dispatcher
 
   val sqs = AmazonSQSClientBuilder.standard
-    .withCredentials(new ProfileCredentialsProvider())
+    .withCredentials(new DefaultAWSCredentialsProviderChain())
     .withRegion(EU_WEST_2)
     .build
 
-  val queueUrl = ""
+  val queueUrl = "xke-test"
 
   def delayedLogging(message: Message): Future[Message] = {
     val promise = Promise[Message]()
@@ -39,14 +39,20 @@ object Application extends App {
     }
   }
 
+  def deleteMessages(messages: Seq[Message]): Try[Int] = {
+    if (messages.isEmpty) Success(0) else {
+      Try(sqs.deleteMessageBatch(
+        queueUrl,
+        messages.map(m => new DeleteMessageBatchRequestEntry(m.getMessageId, m.getReceiptHandle))
+      )).map(_.getSuccessful.size)
+    }
+  }
+
   Source.fromIterator(() => Iterator.continually(sqs.receiveMessage(queueUrl).getMessages.toList))
     .mapConcat(identity)
     .mapAsync(4)(delayedLogging)
     .groupedWithin(10, 5 seconds)
-    .map(messages => sqs.deleteMessageBatch(
-      queueUrl,
-      messages.map(m => new DeleteMessageBatchRequestEntry(m.getMessageId, m.getReceiptHandle)))
-    )
+    .map(deleteMessages)
     .runWith(Sink.ignore)
     .onComplete { done =>
       system.log.info(s"Completed : $done")
--- sqs-java-basic/src/main/java/fr/xebia/sqsjavabasic/Application.java
+++ sqs-java-basic/src/main/java/fr/xebia/sqsjavabasic/Application.java
@@ -1,12 +1,9 @@
 package fr.xebia.sqsjavabasic;
 
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.DeleteMessageBatchRequest;
-import com.amazonaws.services.sqs.model.DeleteMessageBatchRequestEntry;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.amazonaws.services.sqs.model.*;
 
 import java.util.List;
 
@@ -18,13 +15,15 @@ public class Application {
     public static void main(String[] args) throws Exception {
 
         AmazonSQS sqs = AmazonSQSClientBuilder.standard()
-                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(EU_WEST_2)
                 .build();
 
-        String queueUrl = "";
+        String queueUrl = "xke-test";
 
+        System.out.println("Making request to queue '" + queueUrl + "'");
         List<Message> messages = sqs.receiveMessage(new ReceiveMessageRequest(queueUrl)).getMessages();
+        System.out.println("Request returned " + messages.size() + " messages");
         messages.forEach(message -> {
             System.out.println("Message (" + message.getMessageId() + ")");
             System.out.println("    receipt-handle : " + message.getReceiptHandle());
@@ -34,7 +33,11 @@ public class Application {
         List<DeleteMessageBatchRequestEntry> deletions = messages.stream().map(message ->
                 new DeleteMessageBatchRequestEntry(message.getMessageId(), message.getReceiptHandle())
         ).collect(toList());
-        sqs.deleteMessageBatch(new DeleteMessageBatchRequest(queueUrl, deletions));
+        if (! messages.isEmpty()) {
+            System.out.println("Deleting " + messages.size() + " messages from '" + queueUrl + "'");
+            DeleteMessageBatchResult result = sqs.deleteMessageBatch(new DeleteMessageBatchRequest(queueUrl, deletions));
+            System.out.println("Deletion result : " + result.toString());
+        }
 
         sqs.shutdown();
     }
